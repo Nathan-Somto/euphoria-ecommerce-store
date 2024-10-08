ad26d664edd43a7cea9d3ad0b11056b06e6e4197
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-10-08 23:41:16 +0100

    refactor(components/ui): update button secondary variant to match figma design

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/70",
+          "bg-secondary text-primary border border-[#3C4242] hover:bg-[rgba(102,32,193,0.03)] focus-visible:border-primary",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         elevated:"shadow-md "
