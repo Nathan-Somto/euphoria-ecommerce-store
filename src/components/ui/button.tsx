adddc0369066283c4099fce247ae14613b300392
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-10-28 21:53:40 +0100

    refactor(ui/button): update button styles and add loading spinner support

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,13 +3,14 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import { Loader2Icon } from "lucide-react"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:bg-disabled disabled:text-disabled-foreground",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:cursor-no-action disabled:bg-disabled disabled:text-disabled-foreground",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-[#EDEDED] hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
@@ -20,7 +21,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
         neutral: "bg-disabled text-[#807D7E] hover:bg-[#EDEDED]",
         white: "bg-white text-primary-foreground hover:bg-[#F6F6F6]/80",
-        elevated:"shadow-md "
+        elevated: "shadow-md "
       },
       size: {
         default: "h-10 px-4 py-2",
@@ -38,13 +39,30 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  isLoading?: boolean
+  showOnlySpinner?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, showOnlySpinner, isLoading, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
+    if (isLoading) {
+      return (
+        <Comp
+          className={cn(buttonVariants({ variant, size, className }))}
+          ref={ref}
+          disabled
+          {...props}
+        >
+          <div className="animate-spin gap-x-0.5">
+            <Loader2Icon />
+          </div>
+          {showOnlySpinner ? null : props.children}
+        </Comp>
+      )
+    }
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
