8b8553c74b7883b3ce433be6a8af66f3a34d61a7
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-10-28 21:56:08 +0100

    fix(ui/input): resolve  input non destructive border color  on invalid state

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,16 +3,21 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+    isInvalid?: boolean,
+    mainSite?: boolean,
+  }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, isInvalid=false, mainSite=false, type, ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
           "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent outline-none file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
+          className,
+          isInvalid && "!border-destructive !text-destructive placeholder:text-red-400 ring-offset-destructive focus-visible:ring-destructive !focus:ring-destructive",
+          mainSite && "border-[#3C4242] text-[#3C4242] placeholder-text-[#3C4242]"
         )}
         ref={ref}
         {...props}
