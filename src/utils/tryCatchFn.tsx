d775292d95c4eb410e8bd08932cbe7299541eab0
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-10-30 09:53:49 +0100

    feat(tryCatchFn): add optional returnErrorMessage parameter to return error details

--- src/utils/tryCatchFn.tsx
+++ src/utils/tryCatchFn.tsx
@@ -1,9 +1,11 @@
 import { errorLogger } from "./errorLogger";
 type Props<T> = {
     message: string;
-    cb: () => Promise<T>
+    cb: () => Promise<T>;
+    returnErrorMessage?:boolean
 }
 export async function tryCatchFn<T>({
+    returnErrorMessage= false,
     message = "function failed to execute",
     cb
 }: Props<T>) {
@@ -11,6 +13,11 @@ export async function tryCatchFn<T>({
         return { data: await cb() };
     } catch (e) {
         errorLogger(e);
+        if(returnErrorMessage && e instanceof Error){
+            return {
+                message: e.message
+            }
+        }
         return {
             message
         }
