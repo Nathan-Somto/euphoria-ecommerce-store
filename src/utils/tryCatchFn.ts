d56cadfd2255ee4dedda4962285672573ceed2b8
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-10-30 15:31:18 +0100

    feat(tryCatchFn): implement a new tryCatch utility function for error handling

---
+++ src/utils/tryCatchFn.ts
@@ -0,0 +1,41 @@
+import { AuthError } from "next-auth";
+import { errorLogger } from "./errorLogger";
+type Props<T> = {
+    message: string;
+    cb: () => Promise<T>;
+    returnErrorMessage?: boolean
+    throwRedirectError?: boolean
+}
+export async function tryCatchFn<T>({
+    returnErrorMessage = false,
+    message = "function failed to execute",
+    throwRedirectError = false,
+    cb
+}: Props<T>) {
+    try {
+        return { data: await cb() };
+    } catch (e) {
+        errorLogger(e);
+        if (throwRedirectError) {
+            if (e instanceof AuthError) {
+                return {
+                    message: e.message,
+                    error: true,
+                }
+            }
+            // reason for throwing error
+            // https://github.com/nextauthjs/next-auth/discussions/9389
+            throw e;
+        }
+        if (returnErrorMessage && e instanceof Error) {
+            return {
+                message: e.message,
+                error: true
+            }
+        }
+        return {
+            message,
+            error: true
+        }
+    }
+}
