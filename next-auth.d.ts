52272287a7efd92c3d1fc4eb9ce4bf486c411861
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-10-30 21:42:40 +0100

    feat(auth): add TypeScript definitions for NextAuth session and adapter user

---
+++ next-auth.d.ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth"
+import {AdapterUser} from "next-auth/adapters";
+import { Role } from "@prisma/client"
+declare module "next-auth" {
+    interface Session {
+      user: {
+       id: string
+       email: string
+       profilePhoto: string
+       role: Role
+       username: string
+       isEmailVerified: boolean
+       isOAuth: boolean
+      }
+    }
+  }
+declare module "next-auth/adapters" {
+    export interface AdapterUser {
+        username: string
+        name: string
+        id: string
+        image: string | null
+        isEmailVerified: boolean
+        email: string
+        role: Role
+    }
+}
