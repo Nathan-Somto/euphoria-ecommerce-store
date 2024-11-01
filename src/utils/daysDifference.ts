e528073be672ba98c5e764b27475d9448c3891c1
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-11-01 12:04:46 +0100

    fix(utils/daysDifference):  resolve undefined error on getTime function by allowing daysDifference to accept strings

--- src/utils/daysDifference.ts
+++ src/utils/daysDifference.ts
@@ -1,4 +1,15 @@
-export function daysDifference(date1: Date, date2: Date){
-    const miliDiff = date2.getTime() - date1.getTime();
-    return Math.round(miliDiff/ (1000 * 60 * 60 * 24))
+export function daysDifference(date1: Date | string, date2: Date | string): number {
+    let localDate1 = date1;
+    let localDate2 = date2;
+    if (typeof date1 === 'string') {
+        localDate1 = new Date(date1);
+    }
+    if (typeof date2 === 'string') {
+        localDate2 = new Date(date2);
+    }
+    if (localDate1 instanceof Date && localDate2 instanceof Date) {
+        const miliDiff = localDate1.getTime() - localDate2.getTime();
+        return Math.round(miliDiff / (1000 * 60 * 60 * 24))
+    }
+    return 0;
 }
