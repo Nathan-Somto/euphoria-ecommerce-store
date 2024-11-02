ab7597ba1cf6979e704bd62fefa5e2e4d4c749bb
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-11-02 12:58:46 +0100

    feat(product-list): create ProductList component for rendering product cards with animations

---
+++ src/app/(main)/(root)/components/product/product-list.tsx
@@ -0,0 +1,52 @@
+'use client'
+import React from 'react'
+import { motion } from 'framer-motion'
+import { ProductResponse } from '.'
+import ProductCard from './product-card'
+type ProductGridProps = {
+  products: ProductResponse,
+  emptyText?: string
+  emptyTextRender?: (len: number) => React.ReactNode
+}
+function ProductList({ products, emptyText, emptyTextRender }: ProductGridProps) {
+  return (
+    <>
+      {products.length > 0 ? (
+        products.map((product, index) => (
+          <motion.div
+            key={product.id}
+            initial={{ opacity: 0, scale: 0.5 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            viewport={{ amount: 'some', once: true }}
+            transition={{
+              duration: 0.65,
+              delay: 0.75 + (index * 0.09)
+            }}
+          >
+            <ProductCard
+              category={product.category}
+              id={product.id}
+              image={product.image}
+              name={product.name}
+              price={product.price}
+              createdAt={product.createdAt}
+              discountRate={product.discountRate}
+              wishListProductIds={[]}
+            />
+          </motion.div>
+        ))
+      ) : (
+        <p className="text-center text-gray-500  w-full col-span-4">
+          {
+            emptyText
+          }
+          {
+            emptyTextRender && emptyTextRender(products.length)
+          }
+        </p>
+      )}
+    </>
+  )
+}
+
+export default ProductList
