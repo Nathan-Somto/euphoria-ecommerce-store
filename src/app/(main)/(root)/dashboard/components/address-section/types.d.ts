94785b0e319746acf8190cf05d4f632f506aacee
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-11-27 19:42:37 +0100

    feat(dashboard/address): add AddressCard and AddressSection components for showcasing customer addresses

---
+++ src/app/(main)/(root)/dashboard/components/address-section/types.d.ts
@@ -0,0 +1,10 @@
+interface Address {
+    street: string
+    city: string
+    state: string
+    zip: string
+    id: string
+    isDefault: boolean
+    phoneNumber: string
+    country: string
+}
