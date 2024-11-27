d116e94d0215f5b57fa1965ccac13af62a92fe4b
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-11-27 03:36:23 +0100

    feat(components): add support for customizable classnames to LinkHistory and SectionHeading

--- src/app/(main)/(root)/components/link-history.tsx
+++ src/app/(main)/(root)/components/link-history.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { ChevronRight } from 'lucide-react';
 import Link from 'next/link'
+import { cn } from '@/lib/utils';
 type LinkHistoryProps = {
     links: { label: string, href: string }[]
     currentRoute?: string
+    className?: string
+    iconClassName?: string
 }
-export default function LinkHistory({ links, currentRoute }: LinkHistoryProps) {
+export default function LinkHistory({ links, currentRoute, className, iconClassName }: LinkHistoryProps) {
     return (
         <div className='flex items-center'>
             {links.map((link, index) => (
-                <div key={index} className='flex items-center text-lg'>
+                <div key={index} className={cn('flex items-center text-lg', className)}>
                     <Link href={link.href} className={`${currentRoute === link.href ? 'text-primary-foreground' : 'text-secondary-foreground'}`}>
                         {link.label}
                     </Link>
-                    {index < links.length - 1 && <ChevronRight className='w-4 h-4 mx-2' />}
+                    {index < links.length - 1 && <ChevronRight className={cn('size-4 mx-2', iconClassName)} />}
                 </div>
             ))}
         </div>
