83eedd7aa844e20103967524158124c5b44ae072
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-11-27 19:43:36 +0100

    feat(dashboard/layout): wrap layout in SessionProvider for  client session management

--- src/app/(main)/(root)/dashboard/layout.tsx
+++ src/app/(main)/(root)/dashboard/layout.tsx
@@ -1,14 +1,18 @@
 import { PropsWithChildren } from "react";
 import DashboardSidebar from "./components/dashboard-sidebar";
 import { currentSession } from "@/lib/next-auth";
+import { SessionProvider } from "next-auth/react";
 
 export default async function DashboardLayout({ children }: PropsWithChildren) {
     const session = await currentSession()
     return (
-        <div className="max-w-screen-lg mx-auto h-full grid  lg:grid-cols-[250px_1fr]">
+        <SessionProvider>
+        <div className="max-w-screen-lg grid lg:grid-cols-[250px_1fr] mx-auto h-full">
             <DashboardSidebar username={session?.user?.username ?? ''} />
-            <div className="border border-destructive ml-10 lg:ml-0 w-[calc(100%-40px)]">
+            <div className="ml-10  w-[calc(100%-40px)]">
                 {children}
             </div>
-        </div>)
+        </div>
+        </SessionProvider>
+        )
 }
