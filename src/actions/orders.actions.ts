62375a01d18bc15924117aa45ef7e74043539fbe
mkparu somtochi <mkparusomtochi26@gmail.com>
2024-12-14 11:26:32 +0100

    feat(orders): add functions to fetch customer's orders and order details with authentication

--- src/actions/orders.actions.ts
+++ src/actions/orders.actions.ts
@@ -1,5 +1,7 @@
 'use server';
+import { currentSession } from "@/lib/next-auth";
 import prisma from "@/lib/prisma";
+import { clamp } from "@/utils/clamp";
 import { tryCatchFn } from "@/utils/tryCatchFn";
 import { $Enums } from "prisma/prisma-client";
 export async function getAdminOrders(page: number) {
@@ -65,5 +67,141 @@ export async function changeOrderStatus(orderId: string, status: $Enums.Status) 
             });
             return data.status;
         }
+    })
+}
+export async function getCustomersOrders(page: number) {
+    return await tryCatchFn({
+        message: "failed to fetch customer's orders",
+        cb: async () => {
+            const session = await currentSession();
+            if (!session) {
+                throw new Error("user not authenticated")
+            }
+            const totalOrders = await prisma.order.findMany(
+                {
+                    where: {
+                        AND: [
+                            { userId: session.user.id },
+                            {
+                                NOT: {
+                                    status: "PENDING"
+                                }
+                            }
+                        ]
+                    }
+                }
+            );
+            const totalPages = Math.ceil(totalOrders.length / 10)
+            const clampedPage = clamp(0, page - 1, totalPages);
+            const data = await prisma.order.findMany({
+                where: {
+                    AND: [
+                        { userId: session.user.id },
+                        {
+                            NOT: {
+                                status: "PENDING"
+                            }
+                        }
+                    ]
+                },
+                select: {
+                    createdAt: true,
+                    id: true,
+                    status: true,
+                    deliveredTime: true,
+                    failedTime: true,
+                    paidTime: true,
+                    orderedProducts: {
+                        select: {
+                            id: true
+                        }
+                    }
+                },
+                orderBy: {
+                    createdAt: 'desc'
+                },
+                skip: totalPages * clampedPage,
+                take: 10
+            })
+            const formattedData = data.map((item, index) => ({
+                ...item,
+                orderedProducts: item.orderedProducts.map(item => item.id),
+                /* get the order number relative to its page index == 1, page === 1 page size === 10 should give 11  */
+                number: (clampedPage * 10) + index + 1
+            }))
+            return {
+                orders: formattedData,
+                totalPages,
+                page
+            }
+        },
+    })
+}
+export async function getCustomerOrderDetail(orderId: string) {
+    return await tryCatchFn({
+        message: "failed to fetch customer's order",
+        cb: async () => {
+            const session = await currentSession();
+            if (!session) {
+                throw new Error("user not authenticated")
+            }
+            const data = await prisma.order.findFirst({
+                where: {
+                    AND: [
+                        {
+                            id: orderId
+                        },
+                        { userId: session.user.id },
+                        {
+                            NOT: {
+                                status: "PENDING"
+                            }
+                        }
+                    ],
+
+                },
+                select: {
+                    createdAt: true,
+                    id: true,
+                    status: true,
+                    address: true,
+                    paidTime: true,
+                    deliveredTime: true,
+                    orderedProducts: {
+                        select: {
+                            quantity: true,
+                            color: true,
+                            size: true,
+                            Product: {
+                                select: {
+                                    images: true,
+                                    name: true,
+                                    price: true,
+                                    id: true
+                                }
+                            }
+                        }
+                    }
+                }
+            })
+            if (data) {
+                const formattedData = {
+                    ...data,
+                    orderedProducts: data.orderedProducts.map(({ Product, ...item }) => ({
+                        ...item,
+                        image: Product?.images[0] ?? null,
+                        name: Product?.name ?? null,
+                        price: Product?.price ?? null,
+                        id: Product?.id ?? null
+                    })),
+                }
+                return {
+                    order: formattedData,
+                }
+            }
+            return {
+                order: null
+            }
+        },
     })
 }
