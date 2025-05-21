2943e8541a60416b9dd4b53e2927ca688280f97a
Nathan Somto <mkparusomtochi26@gmail.com>
2025-05-21 06:56:04 +0100

    feat(admin-dashboard): refactor revenue and product data actions to match updated schema structure

--- src/app/(admin)/admin/(core)/page.tsx
+++ src/app/(admin)/admin/(core)/page.tsx
@@ -3,81 +3,48 @@ import KpiCard from "./components/kpi-card";
 import { v4 as uuidv4 } from "uuid";
 import RevenueChart from "./components/revenue-chart";
 import ProductChart from "./components/product-chart";
-const data = [
-  {
-    name: "January",
-    total: 23000,
-  },
-  {
-    name: "Feburary",
-    total: 35000,
-  },
-  {
-    name: "March",
-    total: 12500,
-  },
-  {
-    name: "April",
-    total: 1200,
-  },
-  {
-    name: "June",
-    total: 13000
-  },
-  {
-    name: "July",
-    total: 123499
-  },
-  {
-    name: "August",
-    total: 53000
-  },
-  {
-    name: "September",
-    total: 65720
-  },
-  {
-    name: "October",
-    total: 75200
-  },
-  {
-    name: "November",
-    total: 82350
-  },
-  {
-    name: "December",
-    total: 34250
-  }
-];
-const productData = [{
-  name: "White Shirt",
-  total: 15
-}, {
-  name: "Flashy Hats",
-  total: 10
-}, {
-  name: "Sexy Hoodie",
-  total: 4
-},
-{
-  name: "Manly Sneakers",
-  total: 12
-}
-]
-export default function AdminDashboardPage() {
-  const contents = [125000000, 34, 41, 5];
+import { unstable_cache as cache } from "next/cache";
+import {
+  getRevenueData,
+  getProductPurchaseData,
+  getActiveUsers,
+  getProductsInStock,
+  getRevenueChartData,
+  getOrdersAndRevenue
+} from "@/actions/adminDashboard.actions";
+const adminData = cache(() => {
+  return Promise.all([
+    getRevenueData(),
+    getProductPurchaseData(),
+    getActiveUsers(),
+    getProductsInStock(),
+  ]);
+}, ['adminData'], { revalidate: false });
+export default async function AdminDashboardPage() {
+  const [revenueData, purchaseData, activeUsers, productsInStock] = await adminData();
+  const { totalOrders, totalRevenue } = getOrdersAndRevenue(revenueData?.data ?? []);
+  const contents = [
+    totalRevenue,
+    totalOrders,
+    activeUsers?.data ?? 0,
+    productsInStock?.data ?? 0
+  ];
   const refinedData = kpiData.map((item, i) => ({
     ...item,
     content: contents[i],
   }));
+  const revenueChartData = getRevenueChartData(revenueData?.data ?? []);
+  const productData = purchaseData?.data ?? [];
   return (
-    <div className="h-full py-8">
+    <div className="py-8">
       <div className="grid gap-2 mb-20 sm:grid-cols-2  xl:grid-cols-4">
         {refinedData.map((item) => (
           <KpiCard {...item} key={uuidv4()} />
         ))}
       </div>
-      <RevenueChart chartData={data} />
+      <h2 className="text-2xl font-semibold mb-4">Revenue Chart</h2>
+      <RevenueChart chartData={revenueChartData} />
+      <h2 className="text-2xl font-semibold mb-4">Product  Purhcase Chart</h2>
       <ProductChart chartData={productData} />
     </div>
   );
